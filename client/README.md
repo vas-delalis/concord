72e946534f09745ceb6718ad33c891d683124ea6
Vas <vas.delalis@gmail.com>
2022-04-15 21:43:16 +0300

    Initialize app

---
+++ client/README.md
@@ -0,0 +1,3 @@
+# Concord web client
+
+SPA built with React for CEID's Software Engineering class.
---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  min-height: 100vh;
+  display: flex;
+`
+
+const App = () => (
+  <StyledApp>
+    Hello world
+  </StyledApp>
+);
+
+export default App;
