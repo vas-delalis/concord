8ee576c042ff23d6803f788f3e28b3d098872ab7
Vas <vas.delalis@gmail.com>
2022-04-17 16:55:21 +0300

    Create home page main section

--- client/src/features/misc/routes/Home.jsx
+++ client/src/features/misc/routes/Home.jsx
@@ -1,3 +1,52 @@
+import styled from 'styled-components';
+import {
+  SearchIcon,
+} from '@heroicons/react/outline';
+
+const Hero = styled.section`
+  background: linear-gradient(90deg, var(--primary-500) 0%, hsl(255, 70%, 70%) 100%);
+  padding: 48px 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const SearchButton = styled.button`
+  background-color: rgba(255, 255, 255, 0.9);
+  border-radius: 100px;
+  height: 40px;
+  width: 100%;
+  box-shadow: var(--shadow-double);
+  font-weight: 500;
+  font-size: 16px;
+  color: var(--primary-800);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const StyledSearchIcon = styled(SearchIcon)`
+  width: 16px;
+  margin-right: 4px;
+`
+
+const LFGButton = styled.button`
+  margin-top: 32px;
+  border-radius: 5px;
+  background-color: rgba(255, 255, 255, 0.25);
+  color: white;
+  height: 35px;
+  padding: 0px 16px;
+  font-weight: 400;
+  font-size: 16px;
+`
+
 export const Home = () => (
-  <div>App home page</div>
+  <Hero>
+    <SearchButton>
+      <StyledSearchIcon />
+      What would you like to do?
+    </SearchButton>
+    <LFGButton>I'm feeling lucky</LFGButton>
+  </Hero>
 )
