50558eae2a52f05445fcbe107efc60b0b53712b0
Vas <vas.delalis@gmail.com>
2022-04-30 21:36:02 +0300

    Add Tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50:  '#f5f7ff',
+          100: '#e0e4fa',
+          200: '#d3d8f8',
+          300: '#969ced',
+          400: '#6d6dd5',
+          500: '#5b53d0',
+          600: '#4c45b5',
+          700: '#3d36a6',
+          800: '#2c2588',
+          900: '#231e6b'
+        },
+      }
+    },
+  },
+  plugins: [],
+}
