2d92ae978c1cc46104607e547c2f3b174e0a45e3
Vas <vas.delalis@gmail.com>
2022-05-01 12:28:25 +0300

    Refactor ActivitySearch

--- client/src/features/activities/components/ActivitySearch.jsx
+++ client/src/features/activities/components/ActivitySearch.jsx
@@ -5,11 +5,42 @@ import { SearchIcon, ChevronRightIcon } from '@heroicons/react/solid';
 import { Modal, Button } from "components/elements";
 import { getActivities } from "../api/getActivities";
 
+const Search = ({ query, setQuery }) => (
+  <div className='flex items-center pl-3 mb-4 border-b-2 border-gray-400'>
+    <SearchIcon className='h-6 text-gray-500' />
+    <input
+      type="text"
+      placeholder="Search activities"
+      spellCheck="false"
+      value={query}
+      onChange={e => setQuery(e.target.value)}
+      className='p-3 w-full text-gray-700 placeholder:text-gray-400 focus:outline-none'
+    />
+  </div>
+);
+
+const ActivityList = ({ activities, toggleActivitySelect }) => (
+  <ul className='py-4 space-y-0.5'>
+    {activities.slice(0, 5).map(activity => (
+      <article
+        key={activity.id}
+        onClick={() => toggleActivitySelect(activity.id)}
+        className={clsx(
+          'px-6 py-4 rounded',
+          { 'bg-gray-200': activity.selected },
+        )}
+      >
+        {activity.name}
+      </article>
+    ))}
+  </ul>
+);
 
 export const ActivitySearch = ({ isOpen, close }) => {
   const [query, setQuery] = useState('');
   const [activities, setActivities] = useState([]);
 
+  // When query changes, get activities matching query from API
   useEffect(() => {
     setActivities(getActivities(query).map(
       activity => ({
@@ -29,17 +60,7 @@ export const ActivitySearch = ({ isOpen, close }) => {
 
   return (
     <Modal isOpen={isOpen} close={close}>
-      <div className='flex items-center pl-3 mb-4 border-b-2 border-gray-400'>
-        <SearchIcon className='h-6 text-gray-500' />
-        <input
-          type="text"
-          placeholder="Search activities"
-          spellCheck="false"
-          value={query}
-          onChange={e => setQuery(e.target.value)}
-          className='p-3 w-full text-gray-700 placeholder:text-gray-400 focus:outline-none'
-        />
-      </div>
+      <Search query={query} setQuery={setQuery} />
       <Link
         to='groups'
         className={clsx(
@@ -50,21 +71,7 @@ export const ActivitySearch = ({ isOpen, close }) => {
         I'm flexible
         <ChevronRightIcon className='h-7' />
       </Link>
-      <ul className='py-4 space-y-0.5'>
-        {activities.slice(0, 5).map(activity => (
-          <article
-            key={activity.id}
-            onClick={() => toggleActivitySelect(activity.id)}
-            className={clsx(
-              'px-6 py-4 rounded',
-              {'bg-gray-200': activity.selected},
-            )}
-          >
-            {activity.name}
-          </article>
-        ))}
-      </ul>
-
+      <ActivityList activities={activities} toggleActivitySelect={toggleActivitySelect} />
       <Button variant="primary" size="full" disabled={activities.filter(a => a.selected).length === 0}>
         See groups
       </Button>
