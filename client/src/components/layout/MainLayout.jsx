db79b0fc2fc1271a0263c16e6027412a0f85ee52
Vas <vas.delalis@gmail.com>
2022-05-02 19:32:10 +0300

    Improve nav icon style

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -5,20 +5,16 @@ import {
   ChatAltIcon,
   UserCircleIcon
 } from '@heroicons/react/outline';
-import {
-  HomeIcon as SolidHomeIcon,
-  SearchIcon as SolidSearchIcon,
-  ChatAltIcon as SolidChatAltIcon,
-  UserCircleIcon as SolidUserCircleIcon
-} from '@heroicons/react/solid';
+import clsx from "clsx";
 
-const Link = ({ to, IconInactive, IconActive, end=false }) => {
+const Link = ({ to, Icon, end=false }) => {
   return (
     <NavLink to={to} end={end}>
-      {({ isActive }) => isActive ? (
-        <IconActive className='w-6 text-gray-800' />
-      ) : (
-        <IconInactive className='w-6 text-gray-500' />
+      {({ isActive }) => (
+        <Icon className={clsx(
+          'w-6 ',
+          isActive ? 'text-primary-900' : 'text-gray-500'
+        )} />
       )}
     </NavLink>
   )
@@ -30,10 +26,10 @@ export const MainLayout = ({ children }) => (
       {children}
     </main>
     <nav className='fixed bottom-0 left-0 right-0 flex justify-evenly items-center h-14 border-t border-gray-300'>
-      <Link to="/app" IconInactive={HomeIcon} IconActive={SolidHomeIcon} end />
-      <Link to="/app/search" IconInactive={SearchIcon} IconActive={SolidSearchIcon} />
-      <Link to="/app/inbox" IconInactive={ChatAltIcon} IconActive={SolidChatAltIcon} />
-      <Link to="/app/profile" IconInactive={UserCircleIcon} IconActive={SolidUserCircleIcon} />
+      <Link to="/app" Icon={HomeIcon} end />
+      <Link to="/app/search" Icon={SearchIcon} />
+      <Link to="/app/inbox" Icon={ChatAltIcon} />
+      <Link to="/app/profile" Icon={UserCircleIcon} />
     </nav>
   </>
 );
