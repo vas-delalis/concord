dcdf829d68517e7e1c2303d2fca0cd15498e8239
Vas <vas.delalis@gmail.com>
2022-05-03 16:02:13 +0300

    Fix Group tags and add avatar

--- client/src/features/groups/api/fetchGroups.js
+++ client/src/features/groups/api/fetchGroups.js
@@ -1,15 +1,128 @@
 export const fetchGroups = async ({ activities }) => {
   const groups = [
-    { id: 0, activityName: 'Overwatch', title: 'Looking for mercy pocket [silver/gold]', freeSlots: 1, totalSlots: 2 },
-    { id: 1, activityName: 'Overwatch', title: 'rank fr gold - mic 21+ comunication et bonne ambiance lets go', freeSlots: 1, totalSlots: 3 },
-    { id: 2, activityName: 'Overwatch', title: 'PUGs Discord', freeSlots: 2, totalSlots: 6 },
-    { id: 3, activityName: 'Counter-Strike: Global Offensive', title: 'Lassen Sie uns diese Noobs rollen', freeSlots: 1, totalSlots: 3 },
-    { id: 4, activityName: 'Counter-Strike: Global Offensive', title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do', freeSlots: 1, totalSlots: 2 },
-    { id: 5, activityName: 'Counter-Strike: Global Offensive', title: 'Tryhards only', freeSlots: 4, totalSlots: 5 },
-    { id: 6, activityName: 'Counter-Strike: Global Offensive', title: 'παμε', freeSlots: 1, totalSlots: 5 },
-    { id: 7, activityName: 'League of Legends', title: 'League of Legends group', freeSlots: 1, totalSlots: 2 },
-    { id: 8, activityName: 'VALORANT', title: 'VALORANT group', freeSlots: 1, totalSlots: 2 },
-    { id: 9, activityName: 'VALORANT', title: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', freeSlots: 1, totalSlots: 2 },
+    { 
+      id: 0, 
+      activityName: 'Overwatch',
+      activityAlias: 'OW',
+      title: 'Looking for mercy pocket [silver/gold]',
+      freeSlots: 1,
+      totalSlots: 2,
+      language: null,
+      activityAttributes: {
+        voiceChat: true,
+        gamemode: 'Quick Play'
+      }
+    },
+    { 
+      id: 1,
+      activityName: 'Overwatch',
+      activityAlias: 'OW',
+      title: 'rank fr gold - mic 21+ comunication et bonne ambiance lets go', 
+      freeSlots: 1, 
+      totalSlots: 3, 
+      language: 'French',
+      activityAttributes: {
+        voiceChat: true,
+        gamemode: 'Competitive'
+      }
+    },
+    { 
+      id: 2,
+      activityName: 'Overwatch',
+      activityAlias: 'OW',
+      title: 'PUGs Discord', 
+      freeSlots: 2,
+      totalSlots: 6, 
+      language: 'English',
+      activityAttributes: {
+        voiceChat: false,
+        gamemode: null
+      }
+    },
+    { 
+      id: 3,
+      activityName: 'Counter-Strike: Global Offensive',
+      activityAlias: 'CS:GO',
+      title: 'Lassen Sie uns diese Noobs rollen', 
+      freeSlots: 1, 
+      totalSlots: 3, 
+      language: 'German',
+      activityAttributes: {
+        voiceChat: true
+      }
+    },
+    { 
+      id: 4,
+      activityName: 'Counter-Strike: Global Offensive',
+      activityAlias: 'CS:GO',
+      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do', 
+      freeSlots: 1, 
+      totalSlots: 2, 
+      language: null,
+      activityAttributes: {
+        voiceChat: false
+      }
+    },
+    { 
+      id: 5,
+      activityName: 'Counter-Strike: Global Offensive',
+      activityAlias: 'CS:GO',
+      title: 'Tryhards only', 
+      freeSlots: 4, 
+      totalSlots: 5, 
+      language: 'English',
+      activityAttributes: {
+        voiceChat: true
+      }
+    },
+    { 
+      id: 6,
+      activityName: 'Counter-Strike: Global Offensive',
+      activityAlias: 'CS:GO',
+      title: 'παμε', 
+      freeSlots: 1, 
+      totalSlots: 5, 
+      language: 'Greek',
+      activityAttributes: {
+        voiceChat: true
+      }
+    },
+    { 
+      id: 7,
+      activityName: 'League of Legends',
+      activityAlias: 'LoL',
+      title: 'League of Legends group', 
+      freeSlots: 1, 
+      totalSlots: 2, 
+      language: null,
+      activityAttributes: {
+        voiceChat: false
+      }
+    },
+    { 
+      id: 8,
+      activityName: 'VALORANT',
+      activityAlias: null,
+      title: 'VALORANT group', 
+      freeSlots: 1, 
+      totalSlots: 2, 
+      language: null,
+      activityAttributes: {
+        voiceChat: false
+      }
+    },
+    { 
+      id: 9,
+      activityName: 'VALORANT',
+      activityAlias: null,
+      title: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 
+      freeSlots: 1, 
+      totalSlots: 2, 
+      language: 'Portuguese',
+      activityAttributes: {
+        voiceChat: true
+      }
+    },
   ];
 
   if (activities.length === 0) {
--- client/src/features/groups/components/GroupList.jsx
+++ client/src/features/groups/components/GroupList.jsx
@@ -1,17 +1,33 @@
 import { useEffect } from 'react';
 import { useQuery } from 'react-query';
+import { MdMic } from 'react-icons/md' ;
 import { fetchGroups } from '../api/fetchGroups';
+import avatar1 from '@/../../public/avatar1.svg';
+import avatar2 from '@/../../public/avatar2.svg';
 
-const ProfilePic = () => (
-  <div className='rounded-full w-9 h-9 mr-4 bg-primary-200 flex-none' />
-);
+const Avatar = ({ groupId, ownerAvatar }) => {
+  const avatar = groupId % 2 === 0 ? avatar1 : avatar2;
+  return (
+    <img src={avatar} alt='' className='rounded-full w-9 h-9 mr-4 bg-primary-200 flex-none' />
+  );
+};
 
-const Info = ({ title, activityName }) => (
-  <div>
-    <div className='text-gray-900 [overflow-wrap:anywhere]'>{title}</div>
-    <div className='text-gray-500 text-sm'>{activityName}</div>
-  </div>
-);
+const Info = ({ group }) => {
+  const activityName = group.activityAlias ?? group.activityName;
+  const tagArray = [activityName, group.activityAttributes.gamemode, group.language];
+  const tagString = tagArray.filter(tag => tag).join(' · ')
+  return (
+    <div className='space-y-1 shrink'>
+      <div className='text-gray-900 [overflow-wrap:anywhere]'>{group.title}</div>
+      <div className='flex items-center space-x-1 overflow-hidden'>
+        {group.activityAttributes.voiceChat && <MdMic size='20px' className='text-gray-400' />}
+        <div className='text-gray-500 text-sm whitespace-nowrap truncate'>
+          {tagString}
+        </div>
+      </div>
+    </div>
+  )
+};
 
 const Slots = ({ freeSlots, totalSlots }) => (
   <div className='flex items-baseline whitespace-nowrap ml-8 text-gray-500'>
@@ -24,8 +40,8 @@ const Slots = ({ freeSlots, totalSlots }) => (
 const Group = ({ group }) => (
   <article className='p-6 flex items-center justify-between'>
     <div className='flex items-center'>
-      <ProfilePic />
-      <Info title={group.title} activityName={group.activityName} />
+      <Avatar groupId={group.id} ownerAvatar={group.ownerAvatar} />
+      <Info group={group} />
     </div>
     <Slots freeSlots={group.freeSlots} totalSlots={group.totalSlots} />
   </article>
