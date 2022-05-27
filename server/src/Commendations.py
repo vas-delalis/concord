11c790b416a595757e8b43755e5983ce86a6b289
Vas <vas.delalis@gmail.com>
2022-05-27 22:16:35 +0300

    Reformat and restructure files

---
+++ server/src/Commendations.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from User import User
+
+
+class Commendations:
+    commendationsList = None
+
+    def log_commendation(self, commender: User, recipient: User, datetime: datetime):
+        return
+
+    def commend(self, commender: User, recipient: User, datetime: datetime):
+        return
+
+    def commendation_exists(self, commender: User, recipient: User):
+        return
---
+++ server/src/User.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+
+class User:
+    username: str
+    reputation: int
+    history: Any
+    attributes: list
+    activities: list
+
+    def getLastGroup(self):
+        return
+
+    def getHistory(self):
+        return
+
+    def getAttributes(self):
+        return
+
+    def getActivities(self):
+        return
+
+    def increaseReputation(self, amount: int):
+        return
+
+    def isLoggedIn(self):
+        return
+
+    def updateUserData(self):
+        return
+
+    def checkRequirements(self):
+        return
+
+    def sendFriendRequest(self):
+        return
+
+    def addFriend(self):
+        return
