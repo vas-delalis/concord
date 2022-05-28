2843f82ba506aa8d4116c89d1ac4a0ddc5ca93a4
Vas <vas.delalis@gmail.com>
2022-05-28 15:49:56 +0300

    Implement ActivityList.search()

--- server/src/Activity.py
+++ server/src/Activity.py
@@ -2,7 +2,7 @@ class Activity:
     _next_id: int = 0
 
     def __init__(self, name: str, alias: str | None = None):
-        self.id = self._next_id
+        self.id = Activity._next_id
         Activity._next_id += 1
         self.name = name
         self.alias = alias
--- server/src/ActivityList.py
+++ server/src/ActivityList.py
@@ -2,12 +2,26 @@ from Activity import Activity
 
 
 class ActivityList:
-    activities: list[Activity] = [
+    _activities: list[Activity] = [
         Activity("Call of Duty: Warzone", alias="COD"),
         Activity("Counter-Strike: Global Offensive", alias="CSGO"),
         Activity("Dota 2"),
         Activity("League of Legends", alias="LoL"),
     ]
 
+    def get(self, activity_id: int) -> Activity:
+        ...  # <-- This is actually valid Python
+
+    def search(self, query: str) -> list[Activity]:
+        return [
+            activity
+            for activity in self._activities
+            if activity.name.lower().startswith(query.lower())
+            or activity.alias.lower().startswith(query.lower())
+        ]
+
     def get_all(self):
-        return self.activities
+        # When returning a private list, remember to pass a copy instead of the original.
+        # Otherwise, editing the list you get from this method will edit the original field too,
+        # so it wouldn't be private anymore
+        return self._activities.copy()
