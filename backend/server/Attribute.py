5c3de0c354e0ed24dd573e7191876464636074b2
Alexsoil <alexsoilgr@gmail.com>
2022-05-29 16:31:38 +0300

    Implemented Attribute class initialization based on Activity class

--- backend/server/Attribute.py
+++ backend/server/Attribute.py
@@ -1,2 +1,14 @@
+from xmlrpc.client import boolean
+
+
 class Attribute:
-    data = None
+    _next_id: int = 0
+
+    def __init__(self, name, values: tuple):
+        self.id = Attribute._next_id
+        Attribute._next_id += 1
+        self.name = name
+        self.values = values
+
+    def __repr__(self) -> str:
+        return f"<Activity {self.id}: {self.name}>"
--- backend/server/AttributeList.py
+++ backend/server/AttributeList.py
@@ -1,3 +1,9 @@
+from server.Attribute import Attribute
 class AttributeList:
+    class ActivityList:
+        _attributes: list[Attribute] = [
+            Attribute('Gender', ('male', 'female', 'other'))
+        ]
+
     def get_attributes(self):
         return
