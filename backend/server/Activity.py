a23f13d3e53d91464ae61ae90869f4a4b1beef79
Alexsoil <alexsoilgr@gmail.com>
2022-05-30 13:01:02 +0300

    Implemented related attributes per activity

--- backend/server/Activity.py
+++ backend/server/Activity.py
@@ -1,9 +1,11 @@
+from server.Attribute import Attribute
 class Activity:
     _next_id: int = 0
 
-    def __init__(self, name: str, type: str, alias: str | None = None):
+    def __init__(self, name: str, type: str, attributes: list[Attribute],alias: str | None = None):
         self.id = Activity._next_id
         Activity._next_id += 1
+        self._attributes = attributes
         self.name = name
         self.alias = alias
         self.type = type
--- backend/server/ActivityList.py
+++ backend/server/ActivityList.py
@@ -3,20 +3,20 @@ from server.Activity import Activity
 
 class ActivityList:
     _activities: list[Activity] = [
-        Activity("Call of Duty: Warzone", alias="COD", type="Video Games"),
-        Activity("Counter-Strike: Global Offensive", alias="CSGO", type="Video Games"),
-        Activity("Dota 2", type="Video Games"),
-        Activity("League of Legends", alias="LoL", type="Video Games"),
+        Activity("Call of Duty: Warzone", attributes=None , alias="COD", type="Video Games"),
+        Activity("Counter-Strike: Global Offensive", attributes=None, alias="CSGO", type="Video Games"),
+        Activity("Dota 2", attributes=None, type="Video Games"),
+        Activity("League of Legends", attributes=None, alias="LoL", type="Video Games"),
         # Not sure about the wording, feel free to alter it
-        Activity("Football", type="Team Sports"),
-        Activity("Basketball", type="Team Sports"),
-        Activity("Movie Viewing", type="Cultural Event"),
-        Activity("Concert Attendance", type="Cultural Event"),
-        Activity("Dungeons and Dragons", type="Board Games"),
-        Activity("Mountain Biking", type="Extreme Sports"),
-        Activity("Studying", type="Quiet Activities"),
+        Activity("Football", attributes=None, type="Team Sports"),
+        Activity("Basketball", attributes=None, type="Team Sports"),
+        Activity("Movie Viewing", attributes=None, type="Cultural Event"),
+        Activity("Concert Attendance", attributes=None, type="Cultural Event"),
+        Activity("Dungeons and Dragons", attributes=None, type="Board Games"),
+        Activity("Mountain Biking", attributes=None, type="Extreme Sports"),
+        Activity("Studying", attributes=None, type="Quiet Activities"),
         # The infamous "Custom Activity"
-        Activity("Other", type="Unspecified")
+        Activity("Other", attributes=None, type="Unspecified")
     ]
 
     def get(self, activity_id: int) -> Activity:
