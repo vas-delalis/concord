ee007ad3030b88c74105989917fd2142f2d37b9a
Alexsoil <alexsoilgr@gmail.com>
2022-05-30 12:35:43 +0300

    Implemented commendation_exists

--- backend/server/Commendations.py
+++ backend/server/Commendations.py
@@ -16,4 +16,10 @@ class Commendations:
         return
 
     def commendation_exists(self, commender: User, recipient: User) -> bool:
-        ...
+        # Probably could be done in a less cool way xD
+        exists = True
+        try:
+            assert (commender, recipient) in self.commendationsList[:][0:2]
+        except:
+            exists = False
+        return exists
