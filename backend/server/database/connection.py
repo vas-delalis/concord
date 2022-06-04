12fdbbb34b9894793976a37d404d297f0cb4b2f6
Alexsoil <alexsoilgr@gmail.com>
2022-06-04 12:05:24 +0300

    Refactored connection.py, added exceptions

--- backend/server/database/connection.py
+++ backend/server/database/connection.py
@@ -1,7 +1,16 @@
 import sqlite3
+import os
 
 # Return a tuple with the connection object and the cursor object of the concord database
 def concordConnect():
-    sqliteConn = sqlite3.connect('concord_db.db')
-    cursor = sqliteConn.cursor()
-    return (sqliteConn, cursor)
+    path = 'server' + os.path.sep + 'database' + os.path.sep + 'concord_db.db'
+    try:
+        if not os.path.isfile(path):
+            raise Exception("File not found")
+        sqliteConn = sqlite3.connect(path)
+        cursor = sqliteConn.cursor()
+        return (sqliteConn, cursor)
+    except Exception as err:
+        print(err)
+
+cock = concordConnect()
