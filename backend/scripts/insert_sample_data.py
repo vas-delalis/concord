cfe6a3c4a0b04f9dd5d6325a6e7055334d55f05f
Alexsoil <alexsoilgr@gmail.com>
2022-06-11 12:35:28 +0300

    Moved activity initialization to insert_sample_data

--- backend/scripts/insert_sample_data.py
+++ backend/scripts/insert_sample_data.py
@@ -1,8 +1,50 @@
 from server.database.connect import cursor, connection
 
+# Reseting and Importing sample user(s)
 cursor.execute("DELETE FROM user")
 cursor.execute(
     'INSERT INTO user VALUES ("user1", "user1@example.com", "Real Name", "2022-06-06T17:25:17", "test", "Female", 0)'
+)
+# Reseting and Importing sample activities
+cursor.execute("DELETE FROM activity")
+cursor.execute(
+    'INSERT INTO activity VALUES(0, "Call of Duty: Warzone", "Video Games", "CoD")'
+)
+cursor.execute(
+    'INSERT INTO activity VALUES(1, "Counter-Strike: Global Offensive", "Video Games", "CSGO")'
+)
+cursor.execute(
+    'INSERT INTO activity VALUES(2, "Dota 2", "Video Games", NULL)'
+)
+cursor.execute(
+    'INSERT INTO activity VALUES(3, "League of Legends", "Video Games", "LoL")'
+)
+cursor.execute(
+    'INSERT INTO activity VALUES(4, "Overwatch", "Video Games", "OW")'
+)
+cursor.execute(
+    'INSERT INTO activity VALUES(5, "Football", "Team Sports", NULL)'
+)
+cursor.execute(
+    'INSERT INTO activity VALUES(6, "Basketball", "Team Sports", NULL)'
+)
+cursor.execute(
+    'INSERT INTO activity VALUES(7, "Movie Viewing", "Cultural Events", NULL)'
+)
+cursor.execute(
+    'INSERT INTO activity VALUES(8, "Concert Attendance", "Cultural Events", NULL)'
+)
+cursor.execute(
+    'INSERT INTO activity VALUES(9, "Dungeons and Dragons", "Board Games", "DnD")'
+)
+cursor.execute(
+    'INSERT INTO activity VALUES(10, "Mountain Biking", "Extreme Sports", NULL)'
+)
+cursor.execute(
+    'INSERT INTO activity VALUES(11, "Studying", "Quiet Activities", NULL)'
+)
+cursor.execute(
+    'INSERT INTO activity VALUES(12, "Other", "Unspecified", NULL)'
 )
 
 connection.commit()
--- backend/server/ActivityList.py
+++ backend/server/ActivityList.py
@@ -2,26 +2,6 @@ from server.Activity import Activity
 
 
 class ActivityList:
-    _activities: list[Activity] = [
-        Activity("Call of Duty: Warzone", alias="COD", category="Video Games"),
-        Activity(
-            "Counter-Strike: Global Offensive", alias="CSGO", category="Video Games"
-        ),
-        Activity("Dota 2", category="Video Games"),
-        Activity("League of Legends", alias="LoL", category="Video Games"),
-        Activity("Overwatch", alias="OW", category="Video Games"),
-        # Not sure about the wording, feel free to alter it
-        Activity("Football", category="Team Sports"),
-        Activity("Basketball", category="Team Sports"),
-        Activity("Movie Viewing", category="Cultural Event"),
-        Activity("Concert Attendance", category="Cultural Event"),
-        Activity("Dungeons and Dragons", alias="DnD", category="Board Games"),
-        Activity("Mountain Biking", category="Extreme Sports"),
-        Activity("Studying", category="Quiet Activities"),
-        # The infamous "Custom Activity"
-        Activity("Other", category="Unspecified"),
-    ]
-
     def get(self, activity_id: int) -> Activity:
         ...  # <-- This is actually valid Python
 
